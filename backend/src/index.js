db120eca3d41faf1621bd99c548b8a6fbf58d0ac
Karthik2003-cpu <karthikrckm2003@gmail.com>
2025-06-11 15:31:13 +0530

    final changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,9 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }))
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running!");
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
